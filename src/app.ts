220a2208530f8d63ef31b646bc75b14b1e26949a
Kirill Stytsenko <kirill@styts.com>
2024-04-23 15:18:37 +0200

    install some basic dependencies

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from 'express'
+// import { customErrorHandler, unexpectedErrorHandler } from './middleware/errorHandler'
+
+const app = express()
+
+// middlewares
+app.use(express.json())
+
+app.get('/', (_req: Request, res: Response) => {
+  res.json({
+    msg: 'hello world'
+  })
+})
+
+// routes
+// app.use('/auth', require('./views/auth').default)
+// app.use('/database', require('./views/database').default)
+// app.use('/table', require('./views/table').default)
+
+// error handler last
+// app.use(customErrorHandler)
+// app.use(unexpectedErrorHandler)
+
+export default app
