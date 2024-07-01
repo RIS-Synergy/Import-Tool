c8a6a0d0bd25c9787b3da2d46d13ee8679ca3675
Kirill Stytsenko <kirill@styts.com>
2024-07-01 12:46:09 +0200

    PureReferences component

---
+++ components/Pure/Reference.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-dialog>
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn
+        v-if="result"
+        variant="flat"
+        rounded
+        color="#ddd"
+        size="small"
+        class="ma-1"
+        v-bind="activatorProps"
+      >
+        <span v-if="data.systemName === 'Organization'">
+          {{ orgShorten() }}
+        </span>
+        <span v-if="data.systemName === 'User'">
+          {{ result.username }}
+        </span>
+      </v-btn>
+    </template>
+
+    <template v-slot:default="{ isActive }">
+      <v-card>
+        <v-card-text>
+          <Yaml :json="result" />
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text="Close" @click="isActive.value = false"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script setup>
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+});
+
+const { systemName, uuid } = props.data;
+
+const result = ref(null);
+
+function orgShorten () {
+  const { name } = result.value
+  if (!name) return '_'
+  const text = name.en_GB || name.de_DE || '_'
+
+  const firstWordsCut = ['Department of ', 'Institut für ']
+  const rest = firstWordsCut.reduce((acc, cur) => {
+    return acc.replace(cur, '')
+  }, text)
+
+  const numberOfText = 12
+  return rest.length > numberOfText ? rest.slice(0, numberOfText) + '...' : rest
+}
+
+onMounted(() => {
+  $fetch(`/api/ri/reference/${systemName}/${uuid}`).then(
+    (res) => (result.value = res),
+  );
+});
+</script>
+
+<style>
+.v-btn {
+  /* width: 10em; */
+  /* white-space: nowrap;
+     overflow: hidden;
+     text-overflow: ellipsis; */
+  /* max-width: 10em; */
+}
+</style>
