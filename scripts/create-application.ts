9de3214d51e42841ddb8d0f1ea520f8649a63405
Kirill Stytsenko <kirill@styts.com>
2024-07-04 09:13:05 +0200

    Create new script: create-application
    We are now considering to use the clusters: applications and awards,
    besides of just 'projects'

---
+++ scripts/create-application.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { callRIApi } from '../src/utils/ri-api'
+
+// read the json file
+const fs = require('fs');
+const path = require('path');
+const jsonPath = path.join(__dirname, './create-samples/application.json');
+const json = fs.readFileSync(jsonPath, 'utf-8');
+console.log(json);
+
+async function main () {
+  const result = await callRIApi('/applications', 'PUT', JSON.parse(json))
+
+  console.log(result);
+
+  // save the result
+  const resultPath = path.join(__dirname, 'result.json');
+  fs.writeFileSync(resultPath, JSON.stringify(result, null, 2), 'utf-8');
+  console.log(`Result saved to ${resultPath}`);
+}
+main()
