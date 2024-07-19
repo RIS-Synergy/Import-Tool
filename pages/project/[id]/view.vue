7bf5bd104789d847181b01f5716f81999d2d9360
Kirill Stytsenko <kirill@styts.com>
2024-07-19 10:16:01 +0200

    Nicer pure url button (on the right)

--- pages/project/[id]/view.vue
+++ pages/project/[id]/view.vue
@@ -1,6 +1,19 @@
 <template>
   <div v-if="store.pure && store.pure.pureId">
-    <a v-if="store.pure && store.pure.pureId" class="" :href="blankLink" target="_blank">PURE url</a>
+    <div class="ma-3 right">
+      <v-btn
+        v-if="store.pure && store.pure.pureId"
+        class=""
+        variant="flat"
+        color="grey"
+        rounded
+        :href="blankLink"
+        target="_blank"
+      >
+        <v-icon class="mr-2"> mdi-open-in-new </v-icon>
+        PURE url
+      </v-btn>
+    </div>
     <Yaml :json="store.pure" />
   </div>
 </template>
@@ -18,21 +31,26 @@ definePageMeta({
 
 const blankLink = computed(() => {
   const pureId = store.pure.pureId;
-  const link = `https://cris-test.univie.ac.at/admin/editor/dk/atira/pure/modules/unifiedprojectmodel/external/model/project/editor/upmprojecteditor.xhtml?scheme=&type=&id=${pureId}`
-  console.log(link)
-  return link
+  const link = `https://cris-test.univie.ac.at/admin/editor/dk/atira/pure/modules/unifiedprojectmodel/external/model/project/editor/upmprojecteditor.xhtml?scheme=&type=&id=${pureId}`;
+  console.log(link);
+  return link;
 });
 
-
 onMounted(() => {
-  console.log('errors can crash here when opening the monitor after a break')
-  console.log("onMounted with store", store)
+  console.log("errors can crash here when opening the monitor after a break");
+  console.log("onMounted with store", store);
   if (store && store.pure && !store.pure.pureId) {
     loadProject(id, store);
   }
 });
 
 definePageMeta({
-  layout: "project"
+  layout: "project",
 });
 </script>
+
+<style scoped>
+.right {
+  float: right;
+}
+</style>
