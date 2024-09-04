896824315ae0e74c3a6ee627abe809c5a8b0def3
Kirill Stytsenko <kirll@styts.com>
2024-09-04 15:48:20 +0200

    use docker secrets for DATABASE_URL environment variable

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# RIS Synergy import tool
+# RIS Synergy Import Tool
+
+## Setup
+
+Before you run it the first time, initalte a password for the database, and store this under `/secrets`.
+
+```
+openssl rand -base64 32 > secrets/db_password.txt
+```
 
 ## Development
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,19 @@
 import path from "path";
 import dotenv from "dotenv";
+import fs from "fs";
 
 const envFile = path.join(__dirname, process.env.DOTENV_CONFIG_PATH)
 dotenv.config({ path: envFile });
+
+// run it from /run/secrets/db_password and omit the \n
+var db_password = fs.readFileSync('/run/secrets/db_password', 'utf8')
+  .replace(/\n$/, '');
+
+// url-encode the password
+db_password = encodeURIComponent(db_password)
+
+// set the DATABASE_URL that will be used in prisma.schema
+process.env.DATABASE_URL = `postgresql://ris-user:${db_password}@ris-db:5432/ris_db?schema=public`
 
 import app from './app'
 
