719bb6cd6476398d98ad99a2d8727b41f36b7480
Kirill Stytsenko <kirll@styts.com>
2024-09-10 08:39:10 +0200

    Dark/Light mode switch in the UI

--- frontend/components/NavMenu.vue
+++ frontend/components/NavMenu.vue
@@ -6,6 +6,14 @@
     prepend-icon="mdi-google-circles-extended"
   ></v-list-item>
   <v-divider></v-divider>
+  <!-- dark mode switcher -->
+  <v-list-item
+    class="my-2"
+    v-model="theme.global.current.dark"
+    title="Dark Mode"
+    :prepend-icon="prepare_icon"
+    @click="toggleTheme" />
+  <v-divider></v-divider>
   <v-list-item to="/projects" title="Projects" />
   <v-list-item :to="`/templates`" link title="Templates" />
   <v-list-item :to="`/functions`" link title="Functions" />
@@ -15,6 +23,16 @@
   <v-list-item :to="`/pure`" link title="Pure" />
 </template>
 
-
 <script setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+function toggleTheme () {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+
+function prepend_icon() {
+  return theme.global.current.value.dark ? 'mdi-white-balance-sunny' : 'mdi-moon-waning-crescent'
+}
 </script>
