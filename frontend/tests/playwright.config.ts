07840ed5e9b5169eb6a655ca23899c5218fa61e4
Kirill Stytsenko <kirill@styts.com>
2025-08-20 13:05:46 +0200

    Playwright test, while running the frontend service

--- frontend/tests/playwright.config.ts
+++ frontend/tests/playwright.config.ts
@@ -2,19 +2,24 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   reporter: [
-    ['html', { outputFolder: 'test-results' }]
+    ['html', {
+      outputFolder: 'test-results',
+      open: 'never',
+    }]
   ],
   use: {
-    baseURL: 'http://localhost:3032/',
+    baseURL: process.env.PLAYWRIGHT_BASE_URL,
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     ignoreHTTPSErrors: true,
     locale: 'en-US',
     timezoneId: 'Europe/Vienna',
+    actionTimeout: 10000,
   },
   testDir: '../tests',
-  outputDir: '../tests/test-results',
+  // for local: '../tests/test-results',
+  outputDir: process.env.PLAYWRIGHT_OUTPUT_DIR,
   projects: [
     {
       testMatch: '*tests/*.spec.ts',
