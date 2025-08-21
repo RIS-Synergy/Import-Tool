2426ea3910607e22322206efaa05808428f54a39
Kirill Stytsenko <kirill@styts.com>
2025-08-21 13:28:34 +0200

    Make CI workflow reliable
    - Run Playwright tests inside Docker.
    - Persist test reports and logs.
    - Upload artifacts for both tests and logs.
    - Prevent orphan containers from piling up.

--- frontend/tests/playwright.config.ts
+++ frontend/tests/playwright.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
-  reporter: [
-    ['html', {
-      outputFolder: 'test-results',
-      open: 'never',
-    }]
-  ],
+  reporter: [['html', { open: 'never',
+                        // outputFolder: '../playwright-report'
+                      }]],
   use: {
     baseURL: process.env.PLAYWRIGHT_BASE_URL,
     trace: 'retain-on-failure',
@@ -15,11 +12,9 @@ export default defineConfig({
     ignoreHTTPSErrors: true,
     locale: 'en-US',
     timezoneId: 'Europe/Vienna',
-    actionTimeout: 10000,
+    actionTimeout: 30000,
   },
-  testDir: '../tests',
-  // for local: '../tests/test-results',
-  outputDir: process.env.PLAYWRIGHT_OUTPUT_DIR,
+  testDir: '.',
   projects: [
     {
       testMatch: '*tests/*.spec.ts',
