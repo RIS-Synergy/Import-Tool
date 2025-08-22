631a8b39ab02de6c1edd8ffa0c8f1364fbe80665
Kirill Stytsenko <kirill@styts.com>
2025-08-22 16:58:26 +0200

    Frontend to Backend tests is running in Github Actions

--- frontend/tests/example.spec.ts
+++ frontend/tests/example.spec.ts
@@ -6,6 +6,11 @@ test('View the App name text', async ({ page }) => {
   await page.getByText('Import Tool').click();
 });
 
+test('Backend API initial endpoint', async ({ page }) => {
+  await page.goto('/api');
+  await expect(page.getByText('RIS Synergy API')).toBeVisible();
+});
+
 test.skip('Login and view "Projects"', async ({ page }) => {
   await page.getByRole('textbox', { name: 'User name User name' }).fill('admin');
   await page.getByRole('textbox', { name: 'Password Password' }).fill('test123');
--- frontend/tests/playwright.config.ts
+++ frontend/tests/playwright.config.ts
@@ -1,9 +1,25 @@
-import { defineConfig, devices } from '@playwright/test'
+import { defineConfig } from '@playwright/test'
+import { resolve } from 'path';
+import { config } from 'dotenv';
+
+// Determine the env variables source
+if (process.env.NODE_ENV == 'development') {
+  config({ path: resolve(process.cwd(), '.env.dev') });
+  console.log('Using development environment variables from .env.dev');
+  console.log('NODE_ENV', process.env.NODE_ENV)
+  console.log('PLAYWRIGHT_BASE_URL', process.env.PLAYWRIGHT_BASE_URL)
+} else if (process.env.NODE_ENV == 'ci') {
+  console.log('Using CI environment variables from the compose file');
+  console.log('NODE_ENV', process.env.NODE_ENV)
+  console.log('PLAYWRIGHT_BASE_URL', process.env.PLAYWRIGHT_BASE_URL)
+} else {
+  console.log('NODE_ENV', process.env.NODE_ENV)
+  throw new Error('NODE_ENV must be set to `development` or `ci`');
+}
 
 export default defineConfig({
-  reporter: [['html', { open: 'never',
-                        // outputFolder: '../playwright-report'
-                      }]],
+  reporter: [
+    ['html', { open: 'never', }]],
   use: {
     baseURL: process.env.PLAYWRIGHT_BASE_URL,
     trace: 'retain-on-failure',
@@ -12,7 +28,7 @@ export default defineConfig({
     ignoreHTTPSErrors: true,
     locale: 'en-US',
     timezoneId: 'Europe/Vienna',
-    actionTimeout: 30000,
+    actionTimeout: 60000,
   },
   testDir: '.',
   projects: [
