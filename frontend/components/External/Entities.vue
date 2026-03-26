8d565494e72c3648e4c65e0af67cc5c490953a0a
Kirill Stytsenko <github@styts.com>
2026-03-27 00:09:03 +0100

    frontend: show diffs for small v-chips in /projects

--- frontend/components/External/Entities.vue
+++ frontend/components/External/Entities.vue
@@ -12,16 +12,45 @@
       {{ ex.templateType }}
       <span class="ml-1 vertical-hight-bug" v-for="st in ex.SavedTemplate">
         <!-- {{ st.diffList.length }} -->
-        <v-chip v-if="st.diffList.length > 0" size="x-small" color="red">
+        <v-chip
+          v-if="st.diffList.length > 0"
+          size="x-small"
+          color="red"
+          class="cursor-pointer"
+          @click.stop="showDiffs(st.diffList)"
+        >
           <b>
             {{ st.diffList.length }}
           </b>
         </v-chip>
-        <v-chip v-else size="x-small" color="green">
+        <v-chip
+          v-else
+          size="x-small"
+          color="green"
+          class="cursor-pointer"
+          @click.stop="showDiffs(st.diffList)"
+        >
           <v-icon> mdi-check </v-icon>
         </v-chip>
       </span>
     </v-chip>
+
+    <v-dialog v-model="dialog" max-width="1200">
+      <v-card>
+        <v-card-text>
+          <DiffView
+            v-if="selectedDiffList"
+            :diff-list="selectedDiffList"
+            :show-button="false"
+          />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text="Close" @click="dialog = false"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     <pre v-if="false" class="tiny">
       {{ externals }}
     </pre>
@@ -37,6 +66,14 @@ const props = defineProps({
     required: true,
   },
 });
+
+const dialog = ref(false);
+const selectedDiffList = ref(null);
+
+function showDiffs(diffList) {
+  selectedDiffList.value = diffList;
+  dialog.value = true;
+}
 
 function chipClass(ex) {
   return "foo";
@@ -57,8 +94,7 @@ function chipColor(ex) {
 </script>
 
 <style scoped>
-.PROJECT = {
-}
+/* .PROJECT { } */
 
 .tiny {
   font-size: 0.7em;
@@ -66,5 +102,9 @@ function chipColor(ex) {
 
 .vertical-hight-bug {
   margin-bottom: 0.5px;
+}
+
+.cursor-pointer {
+  cursor: pointer;
 }
 </style>
